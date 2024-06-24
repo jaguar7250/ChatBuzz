11aa28579ce49d64714a76ac9e4899c363b10d6c
jaguar7250 <114855508+jaguar7250@users.noreply.github.com>
2024-06-24 17:09:14 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +10,8 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app, server} from "./socket/socket.js"
 const PORT=process.env.PORT||5000;
+
+const _dirname = path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +23,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname,"frontend","dist","index.html"));
+})
 server.listen(PORT,()=> {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)});
