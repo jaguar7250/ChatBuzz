66e0c7bb21f4894337b4fea4c4b68c0854d04cad
jaguar7250 <114855508+jaguar7250@users.noreply.github.com>
2024-06-24 17:41:09 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const _dirname = path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.get("/",(req,res)=>{
-    res.send("Hello world !");
-})
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
